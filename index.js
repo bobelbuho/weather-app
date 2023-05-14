d79736adf1d3cd251770885886a965e1933fab52
bobelbuho <bob.vaz@hotmail.fr>
2023-05-14 23:48:55 +0200

    finish

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ search.addEventListener('click', ()=>{
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-box .humidity');
-            const wind = document.querySelector('weather-box .wind span');
+            const humidity = document.querySelector('.weather-details .humidity');
+            const wind = document.querySelector('weather-details .wind span');
 
             switch(json.weather[0].main){
                 case 'Clear': 
@@ -52,7 +52,20 @@ search.addEventListener('click', ()=>{
                 
             }
 
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>C</span>`
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display ='';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height ='590px';
+
+
+
     })
 
 })
